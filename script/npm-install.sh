8e48aebbeae818196395f65185a049e88c926057
DaRae <momo0735@naver.com>
2021-11-14 22:16:03 +0900

    db 관련 내용 주석처리

--- script/npm-install.sh
+++ script/npm-install.sh
@@ -4,7 +4,7 @@ echo 'Running install_dependencies'
 echo '============================'
 
 /usr/local/bin/pm2 kill
-sudo service mysqld start
-source /home/ubuntu/build/.bash_profile
+# sudo service mysqld start
+# source /home/ubuntu/build/.bash_profile
 cd /home/ubuntu/build/
 npm install
